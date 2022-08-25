8d5ee11e43c9f549521201ff17c7e4464cfb9825
Glanvin <seth.valmy@gmail.com>
2022-08-26 06:57:12 +0800

    Added UIChatHome.java

---
+++ app/src/main/java/me/sera/app/ui/UIChatHome.java
@@ -0,0 +1,16 @@
+package me.sera.app.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import me.sera.app.R;
+
+public class UIChatHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_uichat_home);
+    }
+}
