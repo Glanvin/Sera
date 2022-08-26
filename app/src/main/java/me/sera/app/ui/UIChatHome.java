6a4e567b4d269ed6285ce749ff2d3c87ea3436e6
Glanvin <seth.valmy@gmail.com>
2022-08-26 08:00:56 +0800

    Added a function

--- app/src/main/java/me/sera/app/ui/UIChatHome.java
+++ app/src/main/java/me/sera/app/ui/UIChatHome.java
@@ -3,6 +3,9 @@ package me.sera.app.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.button.MaterialButton;
 
 import me.sera.app.R;
 
@@ -12,5 +15,14 @@ public class UIChatHome extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_uichat_home);
+
+        MaterialButton button = findViewById(R.id.mainui);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
