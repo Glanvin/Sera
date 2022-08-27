d1c318b5eb57844a59ab47e670455f54e2ecad21
Glanvin <seth.valmy@gmail.com>
2022-08-28 01:31:23 +0800

    Added Chat,People and History fragments

---
+++ app/src/main/java/me/sera/app/ui/fragments/Chat.java
@@ -0,0 +1,22 @@
+package me.sera.app.ui.fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import me.sera.app.R;
+
+public class Chat extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_chat, container, false);
+    }
+
+}
