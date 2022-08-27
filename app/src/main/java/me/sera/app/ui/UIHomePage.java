efea09170bd504b44e8bd5e2e95096aa30c931e5
Glanvin <seth.valmy@gmail.com>
2022-08-28 01:32:11 +0800

    Initial commit

---
+++ app/src/main/java/me/sera/app/ui/UIHomePage.java
@@ -0,0 +1,16 @@
+package me.sera.app.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import me.sera.app.R;
+
+public class UIHomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ui_home_page);
+    }
+}
