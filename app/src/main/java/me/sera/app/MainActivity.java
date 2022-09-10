eaa5372a9cb4318bb45ec0e3eb05a39f93099d5f
Glanvin <seth.valmy@gmail.com>
2022-09-10 18:55:54 +0800

    Removed temp Login Function

--- app/src/main/java/me/sera/app/MainActivity.java
+++ app/src/main/java/me/sera/app/MainActivity.java
@@ -3,6 +3,7 @@ package me.sera.app;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -11,7 +12,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.button.MaterialButton;
 
 import me.sera.app.ui.UIHomePage;
-import me.sera.app.ui.UIRegistration;
+import me.sera.app.ui.UILoginRegister;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,41 +22,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        errands();
+        //errands();
+        startActivity(new Intent(MainActivity.this, UILoginRegister.class));
 
     }
 
-
-    private void errands() {
-
-        TextView user = findViewById(R.id.user);
-        TextView pass = findViewById(R.id.pass);
-
-        MaterialButton regis = findViewById(R.id.regist);
-        MaterialButton login = findViewById(R.id.login);
-
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (user.getText().toString().equalsIgnoreCase("Admin")  && pass.getText().toString().equals("admin")) {
-                    startActivity(new Intent(MainActivity.this, UIHomePage.class));
-
-                }else if(user.getText().toString().isEmpty() || pass.getText().toString().isEmpty()){
-                    Toast.makeText(MainActivity.this, "Missing field(s)!", Toast.LENGTH_SHORT).show();
-                    user.setText("");
-                    pass.setText("");
-                } else {
-                    Toast.makeText(MainActivity.this, "Input(s) not valid in!", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-
-        regis.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, UIRegistration.class));
-            }
-        });
-
-    }
 }
