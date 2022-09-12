efdec4509b3cfe997bbe815369cd3990b4c048bc
Glanvin <seth.valmy@gmail.com>
2022-09-13 07:14:58 +0800

    Added Toasty by GrenderG support for better Toast

--- app/src/main/java/me/sera/app/ui/UILogin.java
+++ app/src/main/java/me/sera/app/ui/UILogin.java
@@ -7,6 +7,9 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.button.MaterialButton;
+
+import es.dmoral.toasty.Toasty;
 import me.sera.app.R;
 
 public class UILogin extends AppCompatActivity {
@@ -21,13 +24,38 @@ public class UILogin extends AppCompatActivity {
     }
 
     private void newErrands() {
+
+        String adminUSER = getString(R.string.tempname);
+        String adminPASS = getString(R.string.temppass);
+
         TextView newUSER = (TextView) findViewById(R.id.textView4);
+        TextView email = (TextView) findViewById(R.id.inputEMAIL);
+        TextView password = (TextView) findViewById(R.id.inputPASSWORD);
+
+        MaterialButton login = (MaterialButton) findViewById(R.id.login);
 
         newUSER.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(UILogin.this, UIRegistration.class));
                 finish();
+            }
+        });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(password.getText().toString().trim().isEmpty() && email.getText().toString().trim().isEmpty()) {
+                    Toasty.error(UILogin.this, R.string.login_failed, Toasty.LENGTH_SHORT).show();
+                    return;
+                } else if(!(password.getText().toString().trim().equalsIgnoreCase(adminPASS) && email.getText().toString().trim().equalsIgnoreCase(adminUSER))) {
+                    Toasty.error(UILogin.this, R.string.input_invalid, Toasty.LENGTH_SHORT).show();
+                    return;
+                }
+                if(password.getText().toString().trim().contains(adminPASS) && email.getText().toString().trim().equalsIgnoreCase(adminUSER)) {
+                    startActivity(new Intent(UILogin.this, UIHomePage.class));
+                    finish();
+                }
             }
         });
 
