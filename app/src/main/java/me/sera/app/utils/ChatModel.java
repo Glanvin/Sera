1787ed326c782be97850889bb3e49c550250fa11
Glanvin <seth.valmy@gmail.com>
2022-09-14 13:25:52 +0800

    Added ChatModel and UserModel

---
+++ app/src/main/java/me/sera/app/utils/ChatModel.java
@@ -0,0 +1,22 @@
+package me.sera.app.utils;
+
+public class ChatModel {
+
+    public String sender;
+    public String receiver;
+    public String senderUid;
+    public String receiverUid;
+    public String message;
+    public long timestamp;
+
+    public ChatModel() {}
+
+    public ChatModel(String sender, String receiver, String senderUid, String receiverUid, String message, long timestamp) {
+        this.sender = sender;
+        this.receiver = receiver;
+        this.senderUid = senderUid;
+        this.receiverUid = receiverUid;
+        this.message = message;
+        this.timestamp = timestamp;
+    }
+}
---
+++ app/src/main/java/me/sera/app/utils/UserModel.java
@@ -0,0 +1,15 @@
+package me.sera.app.utils;
+
+public class UserModel {
+
+        public String username;
+        public String email;
+
+        public UserModel() {}
+
+        public UserModel(String username, String email) {
+            this.username = username;
+            this.email = email;
+        }
+
+}
